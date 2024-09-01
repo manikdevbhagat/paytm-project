3ac2421422372be098992113237ffa8e674cd6c9
Manik <manik@mykluster.io>
2024-09-01 17:20:03 +0530

    feat: query balance and txns from db

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -1,8 +1,44 @@
+import { getServerSession } from "next-auth";
 import { AddMoneyForm } from "../../../components/transfer/AddMoneyForm";
 import BalanceTable from "../../../components/transfer/BalanceTable";
 import RecentTransactions from "../../../components/transfer/RecentTransactions";
+import db from "@repo/db/client";
+import { authOptions } from "../../lib/auth";
+import { OnRampTransaction } from "../../../components/transfer/RecentTransactionsTable";
 
-export default function () {
+async function getBalance() {
+  const session = await getServerSession(authOptions);
+  const balance = await db.balance.findFirst({
+    where: {
+      userId: Number(session?.user?.id),
+    },
+  });
+  return {
+    amount: balance?.amount || 0,
+    locked: balance?.locked || 0,
+  };
+}
+
+async function getOnRampTransactions() {
+  const session = await getServerSession(authOptions);
+  const txns = await db.onRampTransaction.findMany({
+    where: {
+      userId: Number(session?.user?.id),
+    },
+  });
+  return txns.map(
+    (txn): OnRampTransaction => ({
+      time: txn.createdAt,
+      amount: txn.amount,
+      status: txn.status,
+      provider: txn.provider,
+    })
+  );
+}
+
+export default async function () {
+  const balance = await getBalance();
+  const onRampTransactions = await getOnRampTransactions();
   return (
     <div className="p-8">
       <div>
@@ -13,8 +49,8 @@ export default function () {
           <AddMoneyForm />
         </div>
         <div className="flex-1 flex flex-col gap-4">
-          <BalanceTable />
-          <RecentTransactions />
+          <BalanceTable amount={balance.amount} locked={balance.locked} />
+          <RecentTransactions transactions={onRampTransactions} />
         </div>
       </div>
     </div>
