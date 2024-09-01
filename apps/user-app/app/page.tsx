cc5adab2ab027c16162a2079ae15cd00f26135bb
Manik <manik@mykluster.io>
2024-09-01 17:21:29 +0530

    feat: add re-direct logic in landing page

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,12 @@
-"use client";
-import { useBalance } from "@repo/store/useBalance";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./lib/auth";
 
-export default function Home() {
-  const balance = useBalance();
-  return <div></div>;
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect("/dashboard");
+  } else {
+    redirect("/api/auth/signin");
+  }
 }
