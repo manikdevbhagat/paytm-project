eb277df7b3d321837335e6a7438df83afb43f605
Manik <manik@mykluster.io>
2024-09-01 16:12:44 +0530

    fix: remove appbar from page.tsx

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,14 +1,7 @@
 "use client";
 import { useBalance } from "@repo/store/useBalance";
-import { Appbar } from "@repo/ui/appbar";
-import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
   const balance = useBalance();
-  const session = useSession();
-  return (
-    <div>
-      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
-    </div>
-  );
+  return <div></div>;
 }
